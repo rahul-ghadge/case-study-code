182414ef03cddd92b6bbe9f6a3b0e1a91f285be5
Rahul Ghadage <54259667+rahul-ghadge@users.noreply.github.com>
2023-06-13 14:34:44 +0530

    Update User_Purchased.py

--- cs-code/User_Purchased.py
+++ cs-code/User_Purchased.py
@@ -1,38 +1,29 @@
 
 import pyspark
-from pyspark.sql import SparkSession
-
-
 import pandas
-
-#pip install openpyxl
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
 
 
 spark = SparkSession.builder.master("local[1]").appName("Test").enableHiveSupport().getOrCreate()
 
-df=spark.sql("select * from hive_input_output.click_stream_json")
 
-#df=spark.read.option("header","true").json("hdfs://0.0.0.0:9000/user/hive/warehouse/hive_input_output.db/click_stream_json/Data.json")
+df=spark.sql("select * from case_study.click_stream_data")
+
+# df=spark.read.option("header","true").json("hdfs://0.0.0.0:9000/user/hive/warehouse/case_study.db/click_stream_data/UserData.json")
 
 df.show()
 
-from pyspark.sql.functions import *
-
-
-df1 = df.withColumn("item", split(df['URL'], 'item.').getItem(1))
-
+df1 = df.withColumn("item", split(df['URL'], 'item').getItem(1))
 
 df1.show()
 
+df1.createOrReplaceTempView("user_purchase")
 
-df1.createOrReplaceTempView("User_Purchase")
-
-df2=spark.sql("select User_Name, count(item) as user_purchased from User_Purchase where Action='purchase' group by User_Name")
+df2=spark.sql("select UserId, count(item) as user_purchased from user_purchase where url LIKE concat('%', 'Purchase', '%') group by UserId")
 
 df2.show()
 
-
 df2.write.mode('overwrite').format('csv').option('header','true').saveAsTable("spark_output.User_Purchased")
 
-
-df2.toPandas().to_excel('/home/suraj/Output/User_Purchased.xlsx', sheet_name = 'Sheet1', index = False)
+df2.toPandas().to_excel('/home/hadoop/case-study/outputs/User_Purchased.xlsx', sheet_name = 'Sheet1', index = False)
