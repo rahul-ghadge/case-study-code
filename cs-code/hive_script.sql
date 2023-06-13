a093e5037bde74d46249ecdaad2fe8f9c2f5bb1c
Rahul Ghadage <54259667+rahul-ghadge@users.noreply.github.com>
2023-06-13 14:36:22 +0530

    Update hive_script.sql

--- cs-code/hive_script.sql
+++ cs-code/hive_script.sql
@@ -1,46 +1,74 @@
-use hive_input_output;
+create database case_study;
 
-drop table if exists click_stream_json;
-create table click_stream_json(
-UserID string, User_Name string, SessionID string, URL string, Action string, LogTime string, PaymentMethod string, LogDate string, Location string)
+use case_study;
+
+
+
+
+drop table if exists click_stream_data;
+
+create table click_stream_data(UserID string, Location string, SessionID string, URL string, PaymentMethod string, LogDate string, LogTime string)
 ROW FORMAT SERDE
   'org.apache.hive.hcatalog.data.JsonSerDe';
+  
+show tables;
 
-load data local inpath'/home/suraj/Data.json' into table click_stream_json;
+load data local inpath '/home/hadoop/case-study/UserData.json' into table click_stream_data;
 
-SET hive.exec.dynamic.partition = true;SET hive.exec.dynamic.partition.mode = nonstrict;
-SET hive.auto.convert.join = false;SET mapreduce.map.memory.mb=8192;SET mapreduce.reduce.memory.mb=8192;
 
-SET hive.exec.max.dynamic.partitions = 10000;
-SET hive.exec.max.dynamic.partitions.pernode = 1000;
+
+drop table if exists most_active_user;
+
+create table most_active_user(UserID string, Total int) row format delimited fields terminated by',' location '/home/hadoop/case-study/outputs/most_active_user';
+
+insert into most_active_user select UserID, count(UserID) as most_active from click_stream_data group by UserID order by most_active desc;
+
+
 SET mapreduce.framework.name=local;
 
---drop table if exists click_stream_par_json;
---create external table click_stream_par_json(
---UserID string, User_Name string, SessionID string, URL string, Action string, LogTime string, PaymentMethod string, LogDate string) 
+
+
+
+--drop table if exists click_stream_partitioned_data;
+--create external table click_stream_partitioned_data(UserID string, SessionID string, URL string, PaymentMethod string, LogDate string, LogTime string)
 --partitioned by (Location string)
 --ROW FORMAT SERDE
 --  'org.apache.hive.hcatalog.data.JsonSerDe';
-
-drop table if exists Most_Active_User;
-
-create table Most_Active_User(User_Name string, Total int) row format delimited fields terminated by',' location '/hive_output/Most_Active_User';
-
-insert into Most_Active_User select User_Name, count(User_Name) as most_active from click_stream_json group by User_Name order by most_active desc;
-
   
---insert into click_stream_par_json partition (Location) select * from click_stream_json;
+--SET hive.exec.dynamic.partition = true;
+--SET hive.exec.dynamic.partition.mode = nonstrict;  
 
---drop table if exists Location_Analysis;
 
---create table Location_Analysis(Location string, No_Of_Users int) row format delimited fields terminated by',' location '/hive_output/Location_Analysis';
+--insert into click_stream_partitioned_data partition (Location) select * from click_stream_data limit 1000;
 
---insert into Location_Analysis select Location, count(UserID) as location_count from click_stream_json group by Location;
 
---drop table if exists Day_Wise_Analysis;
 
---create table Day_Wise_Analysis(Log_Date string, No_Of_Users int) row format delimited fields terminated by',' location '/hive_output/Day_Wise_Analysis';
 
---insert into Day_Wise_Analysis select LogDate, count(UserID) from click_stream_par_json group by LogDate;
+--drop table if exists location_wise_analysis;
+--create table location_wise_analysis(Location string, No_Of_Users int) row format delimited fields terminated by',' location '/home/hadoop/case-study/outputs/location_wise_analysis';
+--insert into location_wise_analysis select Location, count(UserID) as location_count from click_stream_data group by Location;
+
+
+
+--drop table if exists day_wise_count;
+--create table day_wise_count(LogDate string, No_Of_Users int) row format delimited fields terminated by',' location '/home/hadoop/case-study/outputs/day_wise_count';
+--insert into day_wise_count select LogDate, count(UserID) from click_stream_partitioned_data group by LogDate;
+
+
+
+
+
+
+
+
+
+
+SET hive.auto.convert.join = false;
+SET mapreduce.map.memory.mb=8192;
+SET mapreduce.reduce.memory.mb=8192;
+SET hive.exec.max.dynamic.partitions = 10000;
+SET hive.exec.max.dynamic.partitions.pernode = 1000;
+
+
 
 
